4215bb3393117ba64f5eb8585e05178fb545e109
Choi Woojin <cwj7574@gmail.com>
2025-02-25 13:50:34 +0900

    메타태그 검색 seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
-import '@/styles/globals.css'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { siteMetadata } from './metadata'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = siteMetadata
+export const metadata: Metadata = {
+  title: '1도GYM',
+  description: '1도GYM - 미주신경 운동 PT',
+  verification: {
+    google: 'xEuZhAbn3bdB6UvHHM8VCNTNyKFYJ4791H-L7NJvnfc',
+    other: {
+      'naver-site-verification': 'ee33201c27d47e576e6f1a8e7a546c72d1432898'
+    }
+  }
+}
 
 export default function RootLayout({
   children,
@@ -13,6 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta name="google-site-verification" content="xEuZhAbn3bdB6UvHHM8VCNTNyKFYJ4791H-L7NJvnfc" />
+        <meta name="naver-site-verification" content="ee33201c27d47e576e6f1a8e7a546c72d1432898" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
